10de9d6da7a0d183fb58a5bfc57126e8609bb054
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-11-11 17:13:30 +0900

    chore: minimal reproduction

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<ol>
+	<li>Click this <a href="/a#hash">link</a> to goto A</li>
+</ol>
+<ol start="3">
+	<li>Click the browser's forward button</li>
+</ol>
---
+++ src/routes/a/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { page } from '$app/stores';
+
+	$effect(() => {
+		if ($page.url.hash) window.location.hash = $page.url.hash;
+	});
+</script>
+
+<p>You are on A!</p>
+<ol>
+	<li>Now click the browser's back button</li>
+</ol>
